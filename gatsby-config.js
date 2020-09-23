69a865ab0f02459ceebaadc3cfde34c197d957c6
tekjoe <ramirez.josephh@gmail.com>
2020-09-23 16:11:50 -0500

    Fix issue with missing gatsby-plugin-styled-components

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-styled-components`,
     {
       resolve: "gatsby-plugin-react-svg",
       options: {
